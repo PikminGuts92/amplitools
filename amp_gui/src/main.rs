4a9a0e572b933559a39749552530702a060e1d0d
PikminGuts92 <PikminGuts92@users.noreply.github.com>
2023-04-29 23:01:51 -0400

    Parse bank events from midi file

--- amp_gui/src/main.rs
+++ amp_gui/src/main.rs
@@ -1,3 +1,55 @@
+use grim::io::{FileSearchDepth, PathFinder};
+use grim::midi::{MidiEvent, MidiFile, MidiText, MidiTextType};
+use std::collections::HashMap;
+use std::env;
+use std::path::{Path, PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().skip(1).collect();
+
+    if args.len() < 1 {
+        return;
+    }
+
+    let dir_path = Path::new(&args[0]);
+    let file_paths = dir_path.find_files_with_depth(FileSearchDepth::Immediate).unwrap();
+
+    let mid_file_paths = file_paths
+        .iter()
+        .filter(|p| p.file_name()
+            .and_then(|n| n.to_str())
+            .is_some_and(|n| n.ends_with(".mid"))) // Note: is_some_and is 1.70.0 feature
+        .collect::<Vec<_>>();
+
+    println!("Found {} files!", file_paths.len());
+    println!("Found {} midi files!", mid_file_paths.len());
+
+    for mp in mid_file_paths.iter() {
+        let mf = MidiFile::from_path(mp).unwrap();
+
+        let bank_track = mf.tracks
+            .iter()
+            .find(|t| t.name
+                .as_ref()
+                .is_some_and(|n| n.as_str().eq("BANK")))
+            .unwrap();
+
+        let bank_events = bank_track
+            .events
+            .iter()
+            .flat_map(|e| match e {
+                MidiEvent::Meta(mt @ MidiText { pos_realtime: Some(pos), .. }) if mt.is_text()
+                    => Some((*pos, mt.as_str().unwrap())),
+                _ => None
+            })
+            .collect::<Vec<_>>();
+
+        println!("Found {} bank events", bank_events.len());
+
+        for (pos, name) in bank_events.iter() {
+            println!("{pos}ms: {name}");
+        }
+
+        break;
+    }
 }
